30380074bb9d2204477162a470a6e887809d0a61
Apeksha <2023da04200@wilp.bits-pilani.ac.in>
2025-06-21 22:56:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def email_event():
     return jsonify({"status": "success", "reply": reply}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
